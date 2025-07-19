603f87fafe3704556d9959a758cb947e462baacf
hindugulshansingh <152046266+hindugulshansingh@users.noreply.github.com>
2025-07-19 16:02:18 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -1,30 +1,24 @@
-#    This file is part of the Compressor distribution.
-#    Copyright (c) 2021 Danish_00
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation, version 3.
-#
-#    This program is distributed in the hope that it will be useful, but
-#    WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-#    General Public License for more details.
-#
-# License can be found in <
-# https://github.com/1Danish-00/CompressorQueue/blob/main/License> .
+# config.py
 
+import logging
 from decouple import config
+
+# Setup logger
+LOGS = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
 
 try:
     APP_ID = config("APP_ID", cast=int)
     API_HASH = config("API_HASH")
     BOT_TOKEN = config("BOT_TOKEN")
-    DEV = 1287276743
-    OWNER = config("OWNER")
+    DEV = int(config("DEV"))
+    OWNER = config("OWNER").split()
+    
+    # Optional configs
+    THUMB = config("THUMBNAIL", default="thumb.jpg")
     ffmpegcode = ["-preset faster -c:v libx265 -s 854x480 -x265-params 'bframes=8:psy-rd=1:ref=3:aq-mode=3:aq-strength=0.8:deblock=1,1' -metadata 'title=Encoded By AnshuSharma (https://github.com/Anshusharma75/TG-videoCompress)' -pix_fmt yuv420p -crf 30 -c:a libopus -b:a 32k -c:s copy -map 0 -ac 2 -ab 32k -vbr 2 -level 3.1 -threads 1"]
-    THUMB = config("THUMBNAIL")
+
 except Exception as e:
-    LOGS.info("Environment vars Missing")
-    LOGS.info("something went wrong")
-    LOGS.info(str(e))
+    LOGS.error("❌ Environment vars Missing or Invalid!")
+    LOGS.error(str(e))
     exit(1)
